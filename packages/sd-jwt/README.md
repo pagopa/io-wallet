de92210ad99bc3df023941debd7100e87c4cf8c6
Emanuele De Cupis <ema.decu@gmail.com>
2023-06-26 21:21:10 +0200

    [SIW-224] Scaffold sd-jwt package (#3)

---
+++ packages/sd-jwt/README.md
@@ -0,0 +1,3 @@
+# SD-JWT
+A typescript package to verify, read, and present data using SD-JWT format.
+-----
---
+++ packages/sd-jwt/src/__test__/index.test.ts
@@ -0,0 +1,5 @@
+import { it, expect } from "vitest";
+
+it("should work", () => {
+  expect(1).toBe(1);
+});
