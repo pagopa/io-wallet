552d18bfad913955c5a2590fa649a8a045686c09
Emanuele De Cupis <ema.decu@gmail.com>
2023-07-04 13:01:00 +0200

    [SIW-229] Verify token is a valid SD-JWT (#4)
    Co-authored-by: Mario Perrotta <mario.perrotta@pagopa.it>

---
+++ packages/sd-jwt/src/types.ts
@@ -0,0 +1,20 @@
+import * as jwt from "jsonwebtoken";
+
+// Extends Jwt including SD fields
+export type SdJwt = jwt.Jwt & {
+  payload: jwt.Jwt["payload"] & {
+    _sd: string[];
+    _sd_alg?: string;
+  };
+};
+
+export type SupportedAlgorithm =
+  | "RS256"
+  | "RS384"
+  | "RS512"
+  | "ES256"
+  | "ES384"
+  | "ES512"
+  | "PS256"
+  | "PS384"
+  | "PS512";
