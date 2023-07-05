c8f7e7c770993279af3c0f2e9ba5db69cdde0a71
Emanuele De Cupis <ema.decu@gmail.com>
2023-07-05 19:22:34 +0200

    Update packages/react-native-eudiw-pid/src/sd-jwt/index.ts
    Co-authored-by: Mario Perrotta <mario.perrotta@pagopa.it>

--- packages/react-native-eudiw-pid/src/sd-jwt/index.ts
+++ packages/react-native-eudiw-pid/src/sd-jwt/index.ts
@@ -16,7 +16,7 @@ export declare function decode(token: string): Promise<SdJwt4VC>;
  * Verify a token is a valid SD-JWT with disclosures.
  * It verifies the first part is a valid JWT.
  * It also verifies each disclosure is well-formed and its values are consistent
- *  with the claims exposed in the SD-JWT.
+ * with the claims exposed in the SD-JWT.
  * It returns the PID, along with the parsed SD-JWT object and its related disclosures.
  * It throws an error if the validation fails.
  *
