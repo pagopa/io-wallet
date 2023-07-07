3bc6559a46adfd5d07797a03bc3f6c973276d3b7
grausof <grausof@gmail.com>
2023-07-07 15:04:10 +0200

    Add encoder

--- apps/io-func-wallet-solution-backend/src/wallet-instance-attestation.ts
+++ apps/io-func-wallet-solution-backend/src/wallet-instance-attestation.ts
@@ -1,11 +1,32 @@
+import * as t from "io-ts";
+
 import * as TE from "fp-ts/TaskEither";
 import * as RTE from "fp-ts/ReaderTaskEither";
 import { pipe } from "fp-ts/function";
 import { sequenceS } from "fp-ts/lib/Apply";
 
-import { EntityConfigurationEnvironment } from "./entity-configuration";
+import {
+  EntityConfigurationEnvironment,
+  FederationEntity,
+} from "./entity-configuration";
 import { verifyWalletInstanceAttestationRequest } from "./wallet-instance-attestation-request";
 import { LoA, getLoAUri } from "./wallet-provider";
+import { JwkPublicKey } from "./jwk";
+import { WalletInstanceAttestationToJwtModel } from "./encoders/wallet-instance-attestation";
+
+export const WalletInstanceAttestationPayload = t.type({
+  iss: t.string,
+  sub: t.string,
+  type: t.literal("WalletInstanceAttestation"),
+  federationEntity: FederationEntity,
+  asc: t.string,
+  publicJwk: JwkPublicKey,
+  algValueSupported: t.array(t.string),
+});
+
+export type WalletInstanceAttestationPayload = t.TypeOf<
+  typeof WalletInstanceAttestationPayload
+>;
 
 // Build the JWT of the Wallet Instance Attestation given a Wallet Instance Attestation Request
 export const createWalletInstanceAttestation =
@@ -20,8 +41,12 @@ export const createWalletInstanceAttestation =
           verifyWalletInstanceAttestationRequest
         ),
         publicJwk: pipe(signer.getFirstPublicKeyByKty("EC"), TE.fromEither),
+        supportedSignAlgorithms: pipe(
+          signer.getSupportedSignAlgorithms(),
+          TE.fromEither
+        ),
       }),
-      TE.chain(({ request, publicJwk }) =>
+      TE.chain(({ request, publicJwk, supportedSignAlgorithms }) =>
         pipe(
           {
             iss: federationEntityMetadata.basePath.href,
@@ -35,7 +60,10 @@ export const createWalletInstanceAttestation =
               logoUri: federationEntityMetadata.logoUri.href,
             },
             asc: pipe(federationEntityMetadata.basePath, getLoAUri(LoA.basic)),
+            publicJwk,
+            algValueSupported: supportedSignAlgorithms,
           },
+          WalletInstanceAttestationToJwtModel.encode,
           signer.createJwtAndsign({ typ: "va+jwt", x5c: [] }, publicJwk.kid)
         )
       )
