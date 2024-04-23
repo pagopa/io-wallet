34062b00b1204ca33cbcfd256c6266fea9e36c5a
silvicir <127971996+silvicir@users.noreply.github.com>
2024-04-23 09:38:49 +0200

    added tests

--- apps/io-func-wallet-solution-backend/src/infra/http/handlers/__test__/create-wallet-instance.spec.ts
+++ apps/io-func-wallet-solution-backend/src/infra/http/handlers/__test__/create-wallet-instance.spec.ts
@@ -14,7 +14,7 @@ import {
 import { iOSMockData } from "@/infra/attestation-service/ios/__test__/config";
 import { NonceRepository } from "@/nonce";
 
-describe("CreateWalletInstanceHandler", async () => {
+describe("CreateWalletInstanceHandler", () => {
   const { challenge, attestation, keyId } = iOSMockData;
 
   //Create a mock of Wallet Instance Request
@@ -29,7 +29,27 @@ describe("CreateWalletInstanceHandler", async () => {
     delete: () => TE.right(void 0),
   };
 
-  it("should return a 201 HTTP response", async () => {
+  const logger = {
+    log: () => () => {},
+    format: L.format.simple,
+  };
+
+  const attestationServiceConfiguration = {
+    iOsBundleIdentifier:
+      "org.reactjs.native.example.IoReactNativeIntegrityExample",
+    iOsTeamIdentifier: "M2X5YQ4BJ7",
+    androidBundleIdentifier:
+      "org.reactjs.native.example.IoReactNativeIntegrityExample",
+    androidPlayStoreCertificateHash: "",
+    appleRootCertificate: APPLE_APP_ATTESTATION_ROOT_CA,
+    allowDevelopmentEnvironment: true,
+    googlePublicKey: GOOGLE_PUBLIC_KEY,
+    androidCrlUrl: ANDROID_CRL_URL,
+    androidPlayIntegrityUrl: ANDROID_PLAY_INTEGRITY_URL,
+    googleAppCredentialsEncoded: "",
+  };
+
+  it("should return a 204 HTTP response", async () => {
     const handler = CreateWalletInstanceHandler({
       input: pipe(H.request("https://wallet-provider.example.org"), (req) => ({
         ...req,
@@ -37,24 +57,8 @@ describe("CreateWalletInstanceHandler", async () => {
         body: walletInstanceRequest,
       })),
       inputDecoder: H.HttpRequest,
-      logger: {
-        log: () => () => {},
-        format: L.format.simple,
-      },
-      attestationServiceConfiguration: {
-        iOsBundleIdentifier:
-          "org.reactjs.native.example.IoReactNativeIntegrityExample",
-        iOsTeamIdentifier: "M2X5YQ4BJ7",
-        androidBundleIdentifier:
-          "org.reactjs.native.example.IoReactNativeIntegrityExample",
-        androidPlayStoreCertificateHash: "",
-        appleRootCertificate: APPLE_APP_ATTESTATION_ROOT_CA,
-        allowDevelopmentEnvironment: true,
-        googlePublicKey: GOOGLE_PUBLIC_KEY,
-        androidCrlUrl: ANDROID_CRL_URL,
-        androidPlayIntegrityUrl: ANDROID_PLAY_INTEGRITY_URL,
-        googleAppCredentialsEncoded: "",
-      },
+      logger,
+      attestationServiceConfiguration,
       nonceRepository,
     });
 
@@ -68,5 +72,63 @@ describe("CreateWalletInstanceHandler", async () => {
     } = result;
 
     expect(statusCode).toBe(204);
+  });
+
+  it("should return a 422 HTTP response on invalid body", () => {
+    const invalidBody = {
+      foo: "foo",
+    };
+    const handler = CreateWalletInstanceHandler({
+      input: pipe(H.request("https://wallet-provider.example.org"), (req) => ({
+        ...req,
+        method: "POST",
+        body: invalidBody,
+      })),
+      inputDecoder: H.HttpRequest,
+      logger,
+      attestationServiceConfiguration,
+      nonceRepository,
+    });
+
+    expect(handler()).resolves.toEqual(
+      expect.objectContaining({
+        right: expect.objectContaining({
+          statusCode: 422,
+          headers: expect.objectContaining({
+            "Content-Type": "application/problem+json",
+          }),
+        }),
+      })
+    );
+  });
+
+  it("should return a 500 HTTP response on validateChallenge error", () => {
+    const nonceRepositoryThatFailsOnDelete: NonceRepository = {
+      insert: () => TE.left(new Error("not implemented")),
+      delete: () => TE.left(new Error("failed on delete!")),
+    };
+
+    const handler = CreateWalletInstanceHandler({
+      input: pipe(H.request("https://wallet-provider.example.org"), (req) => ({
+        ...req,
+        method: "POST",
+        body: walletInstanceRequest,
+      })),
+      inputDecoder: H.HttpRequest,
+      logger,
+      attestationServiceConfiguration,
+      nonceRepository: nonceRepositoryThatFailsOnDelete,
+    });
+
+    expect(handler()).resolves.toEqual(
+      expect.objectContaining({
+        right: expect.objectContaining({
+          statusCode: 500,
+          headers: expect.objectContaining({
+            "Content-Type": "application/problem+json",
+          }),
+        }),
+      })
+    );
   });
 });
