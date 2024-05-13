4ea83a50a20b3be556fcd2fcc5c5216fa1530fd1
Emanuele De Cupis <ema.decu@gmail.com>
2024-05-13 16:07:50 +0200

    identity

---
+++ infra/identity/prod/westeurope/locals.tf
@@ -0,0 +1,19 @@
+locals {
+  prefix    = "io"
+  env_short = "p"
+  env       = "prod"
+  location  = "westeurope"
+  project   = "${local.prefix}-${local.env_short}"
+  domain    = "wallet"
+
+  repo_name = "io-wallet"
+
+  tags = {
+    CostCenter     = "TS310 - PAGAMENTI & SERVIZI"
+    CreatedBy      = "Terraform"
+    Environment    = "Prod"
+    Owner          = "IO"
+    ManagementTeam = "IO Wallet"
+    Source         = "https://github.com/pagopa/io-wallet"
+  }
+}
