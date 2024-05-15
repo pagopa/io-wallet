c44a82a921680e183d7b07a1d4274abb892f615a
Emanuele De Cupis <ema.decu@gmail.com>
2024-05-15 18:19:18 +0200

    rename tfstate file

--- infra/resources/prod/main.tf
+++ infra/resources/prod/main.tf
@@ -12,7 +12,7 @@ terraform {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfappprodio"
     container_name       = "terraform-state"
-    key                  = "iowallet.resources.prod.westeurope.tfstate"
+    key                  = "iowallet.resources.prod.tfstate"
   }
 }
 
