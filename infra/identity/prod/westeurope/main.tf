5707ac177a11ae09aa3b99f20837b937b410b67f
Emanuele De Cupis <ema.decu@gmail.com>
2024-05-17 13:15:36 +0200

    GitHub federated identity (#57)
    Co-authored-by: christian-calabrese <christian.calabrese@pagopa.it>

---
+++ infra/identity/prod/westeurope/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "<= 3.97.1"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfappprodio"
+    container_name       = "terraform-state"
+    key                  = "iowallet.identity.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+}
+
+module "federated_identities" {
+  source = "github.com/pagopa/dx//infra/modules/azure_federated_identity_with_github?ref=main"
+
+  prefix    = local.prefix
+  env_short = local.env_short
+  env       = local.env
+  domain    = local.domain
+
+  repositories = [local.repo_name]
+
+  tags = local.tags
+}
+
