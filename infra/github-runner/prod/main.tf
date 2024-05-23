5257834990196a2513715c12112f94ed6062ed16
Andrea Grillo <andrea.grillo@pagopa.it>
2024-05-23 23:09:15 +0200

    [EC-297] Add GitHub self-hosted runner (#73)

---
+++ infra/github-runner/prod/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "<= 3.104.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfappprodio"
+    container_name       = "terraform-state"
+    key                  = "iowallet.github-runner.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+}
+
+module "container_app_job_selfhosted_runner" {
+  source = "github.com/pagopa/dx//infra/modules/github_selfhosted_runner_on_container_app_jobs?ref=main"
+
+  prefix    = local.prefix
+  env_short = local.env_short
+
+  repo_name = local.repo_name
+
+  container_app_environment = {
+    name                = "${local.prefix}-${local.env_short}-github-runner-cae"
+    resource_group_name = "${local.prefix}-${local.env_short}-github-runner-rg"
+  }
+
+  key_vault = {
+    name                = "${local.prefix}-${local.env_short}-kv-common"
+    resource_group_name = "${local.prefix}-${local.env_short}-rg-common"
+  }
+
+  tags = local.tags
+}
