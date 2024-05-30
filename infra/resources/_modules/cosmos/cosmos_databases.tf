b18067ff2fbcd4eb56c3c38285d36350c1066a71
silvicir <127971996+silvicir@users.noreply.github.com>
2024-05-30 15:49:14 +0200

    fix

--- infra/resources/_modules/cosmos/cosmos_databases.tf
+++ infra/resources/_modules/cosmos/cosmos_databases.tf
@@ -1,4 +1,4 @@
-resource "azurerm_cosmosdb_sql_database" "user" {
+resource "azurerm_cosmosdb_sql_database" "db" {
   name                = "user"
   resource_group_name = var.resource_group_name
   account_name        = azurerm_cosmosdb_account.wallet.name
