20d7156993b3a8102d483b007ec35bb18b33ab63
silvicir <127971996+silvicir@users.noreply.github.com>
2024-05-30 15:46:23 +0200

    changed database name

--- infra/resources/_modules/cosmos/cosmos_databases.tf
+++ infra/resources/_modules/cosmos/cosmos_databases.tf
@@ -1,5 +1,5 @@
-resource "azurerm_cosmosdb_sql_database" "db" {
-  name                = "db"
+resource "azurerm_cosmosdb_sql_database" "user" {
+  name                = "user"
   resource_group_name = var.resource_group_name
   account_name        = azurerm_cosmosdb_account.wallet.name
 }
