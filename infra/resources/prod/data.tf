95a9cb372c884a1c7700848943b4856af16f7277
Andrea Grillo <andrea.grillo@pagopa.it>
2024-05-30 11:44:06 +0200

    [SIW-1167] Move Function App default key to weu-common Key Vault (#95)

--- infra/resources/prod/data.tf
+++ infra/resources/prod/data.tf
@@ -6,6 +6,11 @@ data "azurerm_resource_group" "weu-common" {
   name = "${local.project_legacy}-rg-common"
 }
 
+data "azurerm_key_vault" "weu-common" {
+  name                = "${local.project_legacy}-kv-common"
+  resource_group_name = data.azurerm_resource_group.weu-common.name
+}
+
 data "azurerm_virtual_network" "vnet_common_itn" {
   name                = "${local.project}-common-vnet-01"
   resource_group_name = "${local.project}-common-rg-01"
