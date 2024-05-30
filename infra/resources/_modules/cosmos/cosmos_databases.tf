09c151deab751fb5094b3c2de969822beb6bd0eb
silvicir <127971996+silvicir@users.noreply.github.com>
2024-05-30 16:34:20 +0200

    revert to db

--- infra/resources/_modules/cosmos/cosmos_databases.tf
+++ infra/resources/_modules/cosmos/cosmos_databases.tf
@@ -1,5 +1,5 @@
 resource "azurerm_cosmosdb_sql_database" "db" {
-  name                = "user"
+  name                = "db"
   resource_group_name = var.resource_group_name
   account_name        = azurerm_cosmosdb_account.wallet.name
 }
