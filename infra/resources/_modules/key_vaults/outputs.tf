12365f90ed0c9644659fb930337faa7d7d80d846
Andrea Grillo <andrea.grillo@pagopa.it>
2024-06-05 21:37:45 +0200

    Assign CosmosDB Contributor role to io-p-adgroup-admin and io-p-adgroup-developers groups (#112)

--- infra/resources/_modules/key_vaults/outputs.tf
+++ infra/resources/_modules/key_vaults/outputs.tf
@@ -5,10 +5,3 @@ output "key_vault_wallet" {
     resource_group_name = azurerm_key_vault.wallet.resource_group_name
   }
 }
-
-output "key_vault_cosmos_key" {
-  value = {
-    id             = azurerm_key_vault_key.cosmos.id
-    versionless_id = azurerm_key_vault_key.cosmos.versionless_id
-  }
-}
