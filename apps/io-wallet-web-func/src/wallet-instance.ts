c3000767bca1b4360feed1a857b503f53cbc8365
silvicir <127971996+silvicir@users.noreply.github.com>
2024-06-24 14:08:10 +0200

    added createWalletInstance function

--- apps/io-wallet-web-func/src/wallet-instance.ts
+++ apps/io-wallet-web-func/src/wallet-instance.ts
@@ -2,8 +2,9 @@ import { IsoDateFromString } from "@pagopa/ts-commons/lib/dates";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import * as O from "fp-ts/Option";
 import * as RTE from "fp-ts/ReaderTaskEither";
+import * as RA from "fp-ts/ReadonlyArray";
 import * as TE from "fp-ts/TaskEither";
-import { pipe } from "fp-ts/function";
+import { flow, pipe } from "fp-ts/function";
 import * as t from "io-ts";
 import { EntityNotFoundError } from "io-wallet-common/http-response";
 import { JwkPublicKey } from "io-wallet-common/jwk";
@@ -17,12 +18,14 @@ const WalletInstanceBase = t.type({
   userId: User.props.id,
 });
 
-const WalletInstanceValid = t.intersection([
+export const WalletInstanceValid = t.intersection([
   WalletInstanceBase,
   t.type({
     isRevoked: t.literal(false),
   }),
 ]);
+
+export type WalletInstanceValid = t.TypeOf<typeof WalletInstanceValid>;
 
 const WalletInstanceRevoked = t.intersection([
   WalletInstanceBase,
@@ -40,12 +43,24 @@ export const WalletInstance = t.union([
 export type WalletInstance = t.TypeOf<typeof WalletInstance>;
 
 export interface WalletInstanceRepository {
+  batchPatch: (
+    operationsInput: {
+      id: string;
+      operations: {
+        op: "add" | "incr" | "remove" | "replace" | "set";
+        path: string;
+        value: unknown;
+      }[];
+    }[],
+    userId: string,
+  ) => TE.TaskEither<Error, void>;
   getAllByUserId: (
     userId: WalletInstance["userId"],
   ) => TE.TaskEither<Error, O.Option<WalletInstance[]>>;
   getLastByUserId: (
     userId: WalletInstance["userId"],
   ) => TE.TaskEither<Error, O.Option<WalletInstance>>;
+  insert: (walletInstance: WalletInstanceValid) => TE.TaskEither<Error, void>;
 }
 
 interface WalletInstanceEnvironment {
@@ -63,5 +78,56 @@ export const getCurrentWalletInstance: (
         TE.fromOption(
           () => new EntityNotFoundError("Wallet instance not found"),
         ),
+      ),
+    );
+
+export const insertWalletInstance: (
+  walletInstance: WalletInstanceValid,
+) => RTE.ReaderTaskEither<WalletInstanceEnvironment, Error, void> =
+  (walletInstance) =>
+  ({ walletInstanceRepository }) =>
+    walletInstanceRepository.insert(walletInstance);
+
+export const revokeUserWalletInstancesExceptOne: (
+  userId: WalletInstance["userId"],
+  walletInstanceId: WalletInstance["id"],
+) => RTE.ReaderTaskEither<WalletInstanceEnvironment, Error, void> =
+  (userId, walletInstanceId) =>
+  ({ walletInstanceRepository }) =>
+    pipe(
+      walletInstanceRepository.getAllByUserId(userId),
+      TE.map(
+        O.fold(
+          () => [],
+          flow(
+            RA.filterMap((walletInstance) =>
+              walletInstance.id !== walletInstanceId
+                ? O.some(walletInstance.id)
+                : O.none,
+            ),
+          ),
+        ),
+      ),
+      TE.chain((walletInstancesId) =>
+        walletInstancesId.length
+          ? walletInstanceRepository.batchPatch(
+              walletInstancesId.map((id) => ({
+                id,
+                operations: [
+                  {
+                    op: "replace",
+                    path: "/isRevoked",
+                    value: true,
+                  },
+                  {
+                    op: "add",
+                    path: "/revokedAt",
+                    value: new Date(),
+                  },
+                ],
+              })),
+              userId,
+            )
+          : TE.right(void 0),
       ),
     );
