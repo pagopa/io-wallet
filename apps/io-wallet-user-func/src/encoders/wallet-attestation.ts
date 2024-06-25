7fe08441c417bf955b0c7fa4509a59a21027f22f
Francesco Grauso <grausof@gmail.com>
2024-06-25 14:50:01 +0200

    fix: Alignment with new technical rules
    Refs: SIW-1279

--- apps/io-wallet-user-func/src/encoders/wallet-attestation.ts
+++ apps/io-wallet-user-func/src/encoders/wallet-attestation.ts
@@ -6,26 +6,27 @@ import { removeTrailingSlash } from "../url";
 import { WalletAttestationPayload } from "../wallet-attestation";
 
 const AlgValueSupported = t.type({
-  alg_values_supported: t.array(t.string),
+  "sd-jwt_alg_values": t.array(t.string),
 });
 
 /* WalletAttestationJwtModel is the model of how the Wallet Attestation JWT
  *is represented outside the code base
  */
 export const WalletAttestationJwtModel = t.type({
-  attested_security_context: t.string,
-  authorization_endpoint: t.literal("eudiw"),
+  aal: t.string,
+  authorization_endpoint: t.literal("eudiw:"),
   cnf: t.type({
     jwk: JwkPublicKey,
   }),
   iss: t.string,
   presentation_definition_uri_supported: t.boolean,
   request_object_signing_alg_values_supported: t.array(t.string),
+  response_modes_supported: t.array(t.string),
   response_types_supported: t.array(t.string),
   sub: t.string,
   vp_formats_supported: t.type({
-    jwt_vc_json: AlgValueSupported,
-    jwt_vp_json: AlgValueSupported,
+    "vc+sd-jwt": AlgValueSupported,
+    "vp+sd-jwt": AlgValueSupported,
   }),
 });
 
@@ -38,15 +39,15 @@ export const WalletAttestationToJwtModel: E.Encoder<
   WalletAttestationPayload
 > = {
   encode: ({
+    aal,
     algValueSupported,
-    attested_security_context,
     federationEntity,
     iss,
     sub,
     walletInstancePublicKey,
   }) => ({
-    attested_security_context,
-    authorization_endpoint: "eudiw",
+    aal,
+    authorization_endpoint: "eudiw:",
     cnf: {
       jwk: walletInstancePublicKey,
     },
@@ -54,14 +55,15 @@ export const WalletAttestationToJwtModel: E.Encoder<
     iss: removeTrailingSlash(iss),
     presentation_definition_uri_supported: false,
     request_object_signing_alg_values_supported: ["ES256"],
+    response_modes_supported: ["form_post.jwt"],
     response_types_supported: ["vp_token"],
     sub: removeTrailingSlash(sub),
     vp_formats_supported: {
-      jwt_vc_json: {
-        alg_values_supported: algValueSupported,
+      "vc+sd-jwt": {
+        "sd-jwt_alg_values": algValueSupported,
       },
-      jwt_vp_json: {
-        alg_values_supported: algValueSupported,
+      "vp+sd-jwt": {
+        "sd-jwt_alg_values": algValueSupported,
       },
     },
   }),
