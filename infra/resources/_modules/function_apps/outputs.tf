bcd81fe74d5cbefcad4750515c853aa5072e19ab
christian-calabrese <christian.calabrese@pagopa.it>
2024-07-17 16:00:15 +0200

    fix: function app output

--- infra/resources/_modules/function_apps/outputs.tf
+++ infra/resources/_modules/function_apps/outputs.tf
@@ -1,10 +1,10 @@
 output "function_app_user" {
   value = {
-    id                    = module.function_app_user.function_app.function_app.id
-    name                  = module.function_app_user.function_app.function_app.name
-    resource_group_name   = module.function_app_user.function_app.resource_group_name
-    principal_id          = module.function_app_user.function_app.function_app.principal_id
-    staging_principal_id  = module.function_app_user.function_app.function_app.slot.principal_id
-    default_hostname = module.function_app_user.function_app.default_hostname
+    id                   = module.function_app_user.function_app.function_app.id
+    name                 = module.function_app_user.function_app.function_app.name
+    resource_group_name  = module.function_app_user.function_app.resource_group_name
+    principal_id         = module.function_app_user.function_app.function_app.principal_id
+    staging_principal_id = module.function_app_user.function_app.function_app.slot.principal_id
+    default_hostname     = module.function_app_user.function_app.function_app.default_hostname
   }
 }
