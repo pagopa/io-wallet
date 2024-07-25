114ed34185b11a9ed8d39444d42678aa392eb4f4
silvicir <127971996+silvicir@users.noreply.github.com>
2024-07-25 12:31:28 +0200

    chore: updated OpenAPI
    Refs: #172

--- apps/io-wallet-user-func/src/infra/azure/functions/create-wallet-attestation.ts
+++ apps/io-wallet-user-func/src/infra/azure/functions/create-wallet-attestation.ts
@@ -1,6 +1,5 @@
+import { CreateWalletAttestationHandler } from "@/infra/http/handlers/create-wallet-attestation";
 import { httpAzureFunction } from "@pagopa/handler-kit-azure-func";
-
-import { CreateWalletAttestationHandler } from "../../http/handlers/create-wallet-attestation";
 
 export const CreateWalletAttestationFunction = httpAzureFunction(
   CreateWalletAttestationHandler,
