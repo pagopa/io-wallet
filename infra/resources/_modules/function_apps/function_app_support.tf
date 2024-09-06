16a6f6c278403a308f642b8db2d6a6fbf12e8d51
silvicir <127971996+silvicir@users.noreply.github.com>
2024-09-06 16:08:24 +0200

    add new function app support-func

---
+++ infra/resources/_modules/function_apps/function_app_support.tf
@@ -0,0 +1,34 @@
+
+module "function_app_support" {
+  source = "github.com/pagopa/dx//infra/modules/azure_function_app?ref=main"
+
+  environment = {
+    prefix          = var.prefix
+    env_short       = var.env_short
+    location        = "italynorth"
+    domain          = "wallet"
+    app_name        = "support"
+    instance_number = "01"
+  }
+
+  resource_group_name = var.resource_group_name
+  health_check_path   = "/api/v1/wallet/health"
+  node_version        = 20
+
+  subnet_cidr                          = var.cidr_subnet_user_func
+  subnet_pep_id                        = var.private_endpoint_subnet_id
+  private_dns_zone_resource_group_name = var.private_dns_zone_resource_group_name
+  virtual_network = {
+    name                = var.virtual_network.name
+    resource_group_name = var.virtual_network.resource_group_name
+  }
+
+  app_settings      = local.function_app_support.app_settings
+  slot_app_settings = local.function_app_support.app_settings
+
+  application_insights_connection_string = var.application_insights_connection_string
+
+  tier = "premium"
+
+  tags = var.tags
+}
--- infra/resources/_modules/function_apps/locals.tf
+++ infra/resources/_modules/function_apps/locals.tf
@@ -62,4 +62,20 @@ locals {
       }
     )
   }
+
+  function_app_support = {
+    app_settings = merge({
+      FUNCTIONS_WORKER_RUNTIME       = "node"
+      FUNCTIONS_WORKER_PROCESS_COUNT = 4
+      NODE_ENV                       = "production"
+
+      // Keepalive fields are all optionals
+      FETCH_KEEPALIVE_ENABLED             = "true"
+      FETCH_KEEPALIVE_SOCKET_ACTIVE_TTL   = "110000"
+      FETCH_KEEPALIVE_MAX_SOCKETS         = "40"
+      FETCH_KEEPALIVE_MAX_FREE_SOCKETS    = "10"
+      FETCH_KEEPALIVE_FREE_SOCKET_TIMEOUT = "30000"
+      FETCH_KEEPALIVE_TIMEOUT             = "60000"
+    })
+  }
 }
