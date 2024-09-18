bd51b4d5efc76e3e89ba746e649c051e05dd1659
silvicir <127971996+silvicir@users.noreply.github.com>
2024-09-18 12:31:26 +0200

    added cosmosdb data reader role to support function app

--- infra/resources/_modules/iam/cosmos_iam.tf
+++ infra/resources/_modules/iam/cosmos_iam.tf
@@ -18,6 +18,26 @@ resource "azurerm_cosmosdb_sql_role_assignment" "func_app_user_staging_slot_to_c
   scope               = "${var.cosmos_db.id}/dbs/${each.value}"
 }
 
+resource "azurerm_cosmosdb_sql_role_assignment" "func_app_support_to_cosmos_data_reader_db" {
+  for_each = var.cosmos_db.database_names
+
+  resource_group_name = var.cosmos_db.resource_group_name
+  account_name        = var.cosmos_db.name
+  role_definition_id  = "${var.cosmos_db.id}/sqlRoleDefinitions/00000000-0000-0000-0000-000000000001"
+  principal_id        = var.function_app.support_func.principal_id
+  scope               = "${var.cosmos_db.id}/dbs/${each.value}"
+}
+
+resource "azurerm_cosmosdb_sql_role_assignment" "func_app_support_staging_slot_to_cosmos_data_reader_db" {
+  for_each = var.function_app.support_func.staging_principal_id == null ? [] : var.cosmos_db.database_names
+
+  resource_group_name = var.cosmos_db.resource_group_name
+  account_name        = var.cosmos_db.name
+  role_definition_id  = "${var.cosmos_db.id}/sqlRoleDefinitions/00000000-0000-0000-0000-000000000001"
+  principal_id        = var.function_app.support_func.staging_principal_id
+  scope               = "${var.cosmos_db.id}/dbs/${each.value}"
+}
+
 resource "azurerm_cosmosdb_sql_role_assignment" "cosmos_contributors" {
   for_each = var.cosmos_db.admin_ids
 
