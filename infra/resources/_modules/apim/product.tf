2df60a69cc4f8fcbfc244caa6e882dd3bc6f3cd5
silvicir <127971996+silvicir@users.noreply.github.com>
2024-09-19 11:48:14 +0200

    added APIM product and API for support API

---
+++ infra/resources/_modules/apim/product.tf
@@ -0,0 +1,14 @@
+module "apim_v2_wallet_support_product" {
+  source = "github.com/pagopa/terraform-azurerm-v3//api_management_product?ref=v8.27.0"
+
+  product_id   = format("%s-wallet-support-api", var.project_legacy)
+  display_name = "IO WALLET API"
+  description  = "Product for support Wallet API"
+
+  api_management_name = var.apim.name
+  resource_group_name = var.apim.resource_group_name
+
+  published             = true
+  subscription_required = true
+  approval_required     = false
+}
