d7274a4b6682e4946f7b6035b96bd02f88f7d31b
silvicir <127971996+silvicir@users.noreply.github.com>
2024-09-23 11:24:26 +0200

    Added APIM product and API for support API
    Refs: #210 SIW-1490

--- infra/resources/_modules/apim/apim.tf
+++ infra/resources/_modules/apim/apim.tf
@@ -1,3 +1,4 @@
+// IO WEB API
 module "apim_v2_web_wallet_api" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3//api_management_api?ref=v8.12.2"
 
@@ -7,7 +8,7 @@ module "apim_v2_web_wallet_api" {
   product_ids           = [var.product_id]
   subscription_required = false
 
-  service_url = format("https://%s/api/v1/wallet", var.user_function.function_hostname)
+  service_url = format("https://%s/api/v1/wallet", var.function_apps.user_function.function_hostname)
 
   description  = "Wallet APIs"
   display_name = "IO Web - Wallet"
@@ -34,4 +35,43 @@ resource "azurerm_api_management_named_value" "user_func_key" {
 data "azurerm_key_vault_secret" "funciowallet_default" {
   name         = "funciowallet-KEY-APPBACKEND"
   key_vault_id = var.key_vault_id
+}
+
+// SUPPORT API
+module "apim_v2_wallet_support_api" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3//api_management_api?ref=v8.12.2"
+
+  name                  = format("%s-wallet-support-api", var.project_legacy)
+  api_management_name   = var.apim.name
+  resource_group_name   = var.apim.resource_group_name
+  product_ids           = [module.apim_v2_wallet_support_product.product_id]
+  subscription_required = true
+
+  service_url = format("https://%s/api/v1/wallet", var.function_apps.support_function.function_hostname)
+
+
+  description  = "API for Wallet Support Assistance"
+  display_name = "IO Wallet - Support"
+  path         = "api/v1/wallet/support"
+  protocols    = ["https"]
+
+  content_format = "openapi"
+
+  content_value = file("${path.module}/api/support-function/_swagger.json")
+
+  xml_content = file("${path.module}/api/support-function/_base_policy.xml")
+}
+
+resource "azurerm_api_management_named_value" "support_func_key" {
+  name                = "io-wallet-support-func-key"
+  api_management_name = var.apim.name
+  resource_group_name = var.apim.resource_group_name
+  display_name        = "io-wallet-support-func-key"
+  value               = data.azurerm_key_vault_secret.support_func_key_default.value
+  secret              = "true"
+}
+
+data "azurerm_key_vault_secret" "support_func_key_default" {
+  name         = "io-wallet-support-func-key"
+  key_vault_id = var.key_vault_wallet_id
 }
