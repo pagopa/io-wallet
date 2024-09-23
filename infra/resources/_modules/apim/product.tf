109128d8203841c1f81d69db43140d40e84d86ac
silvicir <127971996+silvicir@users.noreply.github.com>
2024-09-23 09:39:11 +0200

    changed APIM product name

--- infra/resources/_modules/apim/product.tf
+++ infra/resources/_modules/apim/product.tf
@@ -2,7 +2,7 @@ module "apim_v2_wallet_support_product" {
   source = "github.com/pagopa/terraform-azurerm-v3//api_management_product?ref=v8.27.0"
 
   product_id   = format("%s-wallet-support-api", var.project_legacy)
-  display_name = "IO WALLET API"
+  display_name = "IO WALLET SUPPORT API"
   description  = "Product for support Wallet API"
 
   api_management_name = var.apim.name
