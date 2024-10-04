b9510c0ce6acbf1c246718ab955dd82bc95e09b1
Andrea Grillo <andrea.grillo@pagopa.it>
2024-10-04 08:35:07 +0200

    [CES-150] Add IAM roles to Function App User 02 (#230)

--- infra/resources/_modules/function_apps/outputs.tf
+++ infra/resources/_modules/function_apps/outputs.tf
@@ -9,6 +9,17 @@ output "function_app_user" {
   }
 }
 
+output "function_app_user_02" {
+  value = {
+    id                   = module.function_app_user_02.function_app.function_app.id
+    name                 = module.function_app_user_02.function_app.function_app.name
+    resource_group_name  = module.function_app_user_02.function_app.resource_group_name
+    principal_id         = module.function_app_user_02.function_app.function_app.principal_id
+    staging_principal_id = module.function_app_user_02.function_app.function_app.slot.principal_id
+    default_hostname     = module.function_app_user_02.function_app.function_app.default_hostname
+  }
+}
+
 output "function_app_support" {
   value = {
     name                 = module.function_app_support.function_app.function_app.name
