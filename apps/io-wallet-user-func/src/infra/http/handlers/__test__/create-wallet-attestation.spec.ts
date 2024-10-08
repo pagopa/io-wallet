d9d8cfb72eee1310d033698af539042eb62e788c
grausof <grausof@gmail.com>
2024-10-08 12:50:57 +0200

    Fix test

--- apps/io-wallet-user-func/src/infra/http/handlers/__test__/create-wallet-attestation.spec.ts
+++ apps/io-wallet-user-func/src/infra/http/handlers/__test__/create-wallet-attestation.spec.ts
@@ -4,6 +4,7 @@ import {
   ANDROID_PLAY_INTEGRITY_URL,
   APPLE_APP_ATTESTATION_ROOT_CA,
   GOOGLE_PUBLIC_KEY,
+  HARDWARE_PUBLIC_TEST_JWK,
 } from "@/app/config";
 import { iOSMockData } from "@/infra/attestation-service/ios/__test__/config";
 import { NonceRepository } from "@/nonce";
@@ -46,6 +47,7 @@ const attestationServiceConfiguration = {
   appleRootCertificate: APPLE_APP_ATTESTATION_ROOT_CA,
   googleAppCredentialsEncoded: "",
   googlePublicKey: GOOGLE_PUBLIC_KEY,
+  hardwarePublicTestJwk: HARDWARE_PUBLIC_TEST_JWK,
   httpRequestTimeout: 0,
   iOsTeamIdentifier: "M2X5YQ4BJ7",
   iosBundleIdentifiers: [
