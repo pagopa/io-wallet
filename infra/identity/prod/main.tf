e37ba001c5995162b4efdad5761fe957576a97bf
Andrea Grillo <andrea.grillo@pagopa.it>
2024-10-08 17:24:55 +0200

    add identities to repo

--- infra/identity/prod/main.tf
+++ infra/identity/prod/main.tf
@@ -102,3 +102,48 @@ resource "azurerm_key_vault_access_policy" "cd" {
 
   secret_permissions = ["Get", "List", "Set"]
 }
+
+module "opex_federated_identities" {
+  source = "github.com/pagopa/dx//infra/modules/azure_federated_identity_with_github?ref=main"
+
+  prefix    = local.prefix
+  env_short = local.env_short
+  env       = "opex-${local.env}"
+  domain    = "${local.domain}-opex"
+
+  repositories = [local.repo_name]
+
+  continuos_integration = {
+    enable = true
+    roles = {
+      subscription = ["Reader"]
+      resource_groups = {
+        dashboards = [
+          "Reader"
+        ],
+        terraform-state-rg = [
+          "Storage Blob Data Reader",
+          "Reader and Data Access"
+        ]
+      }
+    }
+  }
+
+  continuos_delivery = {
+    enable = true
+    roles = {
+      subscription = ["Reader"]
+      resource_groups = {
+        dashboards = [
+          "Contributor"
+        ],
+        terraform-state-rg = [
+          "Storage Blob Data Contributor",
+          "Reader and Data Access"
+        ]
+      }
+    }
+  }
+
+  tags = local.tags
+}
--- infra/repository/data.tf
+++ infra/repository/data.tf
@@ -13,6 +13,16 @@ data "azurerm_user_assigned_identity" "identity_app_prod_cd" {
   resource_group_name = local.identity_resource_group_name
 }
 
+data "azurerm_user_assigned_identity" "identity_opex_prod_ci" {
+  name                = "${local.project}-wallet-opex-github-ci-identity"
+  resource_group_name = local.identity_resource_group_name
+}
+
+data "azurerm_user_assigned_identity" "identity_opex_prod_cd" {
+  name                = "${local.project}-wallet-opex-github-cd-identity"
+  resource_group_name = local.identity_resource_group_name
+}
+
 data "github_organization_teams" "all" {
   root_teams_only = true
   summary_only    = true
