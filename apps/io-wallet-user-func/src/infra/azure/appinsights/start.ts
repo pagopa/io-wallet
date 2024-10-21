d313161037456fb9d24c19013471182e81421ad9
silvicir <127971996+silvicir@users.noreply.github.com>
2024-10-21 16:01:15 +0200

    chore: update applicationinsights setting (#252)
    Refs: #252 SIW-1774

--- apps/io-wallet-user-func/src/infra/azure/appinsights/start.ts
+++ apps/io-wallet-user-func/src/infra/azure/appinsights/start.ts
@@ -1,6 +1,8 @@
 import * as ai from "applicationinsights";
 
 ai.setup(process.env["AppInsightsConnectionString"])
+  .setAutoCollectRequests(true)
+  .setAutoCollectExceptions(true)
   .setUseDiskRetryCaching(true)
   .start();
 
