0b19aebae29d6a5cf1e205e85e38db39ac0cd9f0
silvicir <127971996+silvicir@users.noreply.github.com>
2024-10-28 09:00:24 +0100

    fix lint

--- apps/io-wallet-user-func/src/infra/azure/appinsights/start.ts
+++ apps/io-wallet-user-func/src/infra/azure/appinsights/start.ts
@@ -1,16 +1,16 @@
-import * as ai from "applicationinsights";
-import { UndiciInstrumentation } from "@opentelemetry/instrumentation-undici";
-import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { metrics, trace } from "@opentelemetry/api";
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import { UndiciInstrumentation } from "@opentelemetry/instrumentation-undici";
+import * as ai from "applicationinsights";
 
 ai.setup(process.env["AppInsightsConnectionString"])
   .setUseDiskRetryCaching(true)
   .start();
 
 registerInstrumentations({
-  tracerProvider: trace.getTracerProvider(),
-  meterProvider: metrics.getMeterProvider(),
   instrumentations: [new UndiciInstrumentation()],
+  meterProvider: metrics.getMeterProvider(),
+  tracerProvider: trace.getTracerProvider(),
 });
 
 export default ai;
