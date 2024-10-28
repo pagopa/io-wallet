e1ef3be342ead0c88e89e117fd43a3134e2e891c
silvicir <127971996+silvicir@users.noreply.github.com>
2024-10-28 08:55:02 +0100

    add fetch instrumentation

--- apps/io-wallet-user-func/src/infra/azure/appinsights/start.ts
+++ apps/io-wallet-user-func/src/infra/azure/appinsights/start.ts
@@ -1,7 +1,16 @@
 import * as ai from "applicationinsights";
+import { UndiciInstrumentation } from "@opentelemetry/instrumentation-undici";
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import { metrics, trace } from "@opentelemetry/api";
 
 ai.setup(process.env["AppInsightsConnectionString"])
   .setUseDiskRetryCaching(true)
   .start();
+
+registerInstrumentations({
+  tracerProvider: trace.getTracerProvider(),
+  meterProvider: metrics.getMeterProvider(),
+  instrumentations: [new UndiciInstrumentation()],
+});
 
 export default ai;
