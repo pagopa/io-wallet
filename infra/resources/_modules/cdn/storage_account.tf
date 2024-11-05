d9195adb37e62d868ce32ef6cdaa24910bbcd29a
silvicir <127971996+silvicir@users.noreply.github.com>
2024-11-05 12:14:25 +0100

    added jwt validation containers

--- infra/resources/_modules/cdn/storage_account.tf
+++ infra/resources/_modules/cdn/storage_account.tf
@@ -5,7 +5,6 @@ resource "azurerm_storage_account" "this" {
   account_tier                    = "Standard"
   account_kind                    = "StorageV2"
   account_replication_type        = "ZRS"
-  enable_https_traffic_only       = true
   allow_nested_items_to_be_public = true
   public_network_access_enabled   = true
   shared_access_key_enabled       = false
@@ -25,6 +24,30 @@ resource "azurerm_storage_account" "this" {
 
 resource "azurerm_storage_container" "well_known" {
   name                 = "well-known"
+  storage_account_name = azurerm_storage_account.this.name
+
+  # tfsec:ignore:azure-storage-no-public-access
+  container_access_type = "container"
+}
+
+resource "azurerm_storage_container" "exchange" {
+  name                 = "exchange"
+  storage_account_name = azurerm_storage_account.this.name
+
+  # tfsec:ignore:azure-storage-no-public-access
+  container_access_type = "container"
+}
+
+resource "azurerm_storage_container" "hub-spid-login" {
+  name                 = "hub-spid-login"
+  storage_account_name = azurerm_storage_account.this.name
+
+  # tfsec:ignore:azure-storage-no-public-access
+  container_access_type = "container"
+}
+
+resource "azurerm_storage_container" "pdnd" {
+  name                 = "pdnd"
   storage_account_name = azurerm_storage_account.this.name
 
   # tfsec:ignore:azure-storage-no-public-access
