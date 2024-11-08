7fb040038b1e8db8772411000e291daf47dd4b8a
silvicir <127971996+silvicir@users.noreply.github.com>
2024-11-08 16:01:55 +0100

    updated storage account name

--- infra/resources/_modules/storage/storage_accounts.tf
+++ infra/resources/_modules/storage/storage_accounts.tf
@@ -1,12 +1,12 @@
 resource "azurerm_storage_account" "wallet_revocation_storage" {
-  name                            = "${var.project}-wallet-revocation-storage-01"
-  resource_group_name             = var.resource_group_name
-  location                        = var.location
-  account_tier                    = "Standard"
-  account_kind                    = "StorageV2"
-  account_replication_type        = "ZRS"
-  shared_access_key_enabled       = false
-  min_tls_version                 = "TLS1_2"
+  name                      = "${replace(var.project, "-", "")}walletrevocationstorage01"
+  resource_group_name       = var.resource_group_name
+  location                  = var.location
+  account_tier              = "Standard"
+  account_kind              = "StorageV2"
+  account_replication_type  = "ZRS"
+  shared_access_key_enabled = false
+  min_tls_version           = "TLS1_2"
 
   tags = var.tags
 }
