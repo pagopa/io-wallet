bc613212b7734874667e8d2e640f49970c39059e
grausof <grausof@gmail.com>
2024-11-10 17:16:40 +0100

    Add telemetry

--- apps/io-wallet-user-func/src/infra/azure/functions/validate-wallet-instance-attested-key.ts
+++ apps/io-wallet-user-func/src/infra/azure/functions/validate-wallet-instance-attested-key.ts
@@ -1,6 +1,19 @@
 import { ValidateWalletInstanceAttestedKeyHandler } from "@/infra/handlers/validate-wallet-instance-attested-key";
 import { azureFunction } from "@pagopa/handler-kit-azure-func";
+import { pipe } from "fp-ts/function";
+import * as RTE from "fp-ts/lib/ReaderTaskEither";
+import { sendTelemetryException } from "io-wallet-common/infra/azure/appinsights/telemetry";
 
 export const ValidateWalletInstanceAttestedKeyFunction = azureFunction(
-  ValidateWalletInstanceAttestedKeyHandler,
+  pipe(
+    ValidateWalletInstanceAttestedKeyHandler,
+    RTE.orElseFirstW((error) =>
+      pipe(
+        sendTelemetryException(error, {
+          functionName: "validateWalletInstance",
+        }),
+        RTE.fromReader,
+      ),
+    ),
+  ),
 );
