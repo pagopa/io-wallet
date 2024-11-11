306709c7b667552073729ce340b914a81a8bb0d4
grausof <grausof@gmail.com>
2024-11-11 14:07:51 +0100

    Fix handler

--- apps/io-wallet-user-func/src/infra/azure/functions/validate-wallet-instance-attested-key.ts
+++ apps/io-wallet-user-func/src/infra/azure/functions/validate-wallet-instance-attested-key.ts
@@ -1,19 +1,6 @@
 import { ValidateWalletInstanceAttestedKeyHandler } from "@/infra/handlers/validate-wallet-instance-attested-key";
 import { azureFunction } from "@pagopa/handler-kit-azure-func";
-import { pipe } from "fp-ts/function";
-import * as RTE from "fp-ts/lib/ReaderTaskEither";
-import { sendTelemetryException } from "io-wallet-common/infra/azure/appinsights/telemetry";
 
 export const ValidateWalletInstanceAttestedKeyFunction = azureFunction(
-  pipe(
-    ValidateWalletInstanceAttestedKeyHandler,
-    RTE.orElseFirstW((error) =>
-      pipe(
-        sendTelemetryException(error, {
-          functionName: "validateWalletInstance",
-        }),
-        RTE.fromReader,
-      ),
-    ),
-  ),
+  ValidateWalletInstanceAttestedKeyHandler,
 );
