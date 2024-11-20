c18a2e6af5763a6ff7b3ef129fb760e37eae8afd
grausof <grausof@gmail.com>
2024-11-20 09:28:09 +0100

    Remove unused algs

--- apps/io-wallet-user-func/src/infra/crypto/__test__/signer.spec.ts
+++ apps/io-wallet-user-func/src/infra/crypto/__test__/signer.spec.ts
@@ -82,18 +82,7 @@ describe("CryptoSigner", () => {
 
     expect(result).toEqual({
       _tag: "Right",
-      right: [
-        "ES256",
-        "ES256K",
-        "ES384",
-        "ES512",
-        "RS256",
-        "RS384",
-        "RS512",
-        "PS256",
-        "PS384",
-        "PS512",
-      ],
+      right: ["ES256"],
     });
   });
 
