c6afbfc9d709b9326aa4b456484754e779263974
Andrea Grillo <andrea.grillo@outlook.com>
2024-11-22 09:23:39 +0100

    add lock and update id name

--- infra/resources/_modules/ids/psn_identity.tf
+++ infra/resources/_modules/ids/psn_identity.tf
@@ -1,7 +1,18 @@
 resource "azurerm_user_assigned_identity" "psn_01" {
-  name                = "${var.project}-wallet-cosno-psn-id-01"
+  name                = "${var.project}-wallet-psn-id-01"
   resource_group_name = var.resource_group_name
   location            = var.location
 
   tags = var.tags
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_management_lock" "psn_id_01" {
+  name       = azurerm_user_assigned_identity.psn_01.name
+  scope      = azurerm_user_assigned_identity.psn_01.id
+  lock_level = "CanNotDelete"
+  notes      = "This Managed Identity is federated with PSN tenant and is required to read data stored in services using PSN's CMK"
 }
