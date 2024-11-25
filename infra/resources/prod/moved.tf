aa9562fe573e1742dbc0c248f8e45273960c3254
Andrea Grillo <andrea.grillo@outlook.com>
2024-11-25 14:58:34 +0100

    move leases definition outside

---
+++ infra/resources/prod/moved.tf
@@ -0,0 +1,31 @@
+## ioweb
+
+# moved {
+#   from = module.apim.module.apim_v2_web_wallet_api.azurerm_api_management_api_policy.this[0]
+#   to   = module.apim.azurerm_api_management_api.io_web
+# }
+
+# moved {
+#   from = module.apim_itn.module.apim_v2_web_wallet_api.azurerm_api_management_api_policy.this[0]
+#   to   = module.apim_itn.azurerm_api_management_api.io_web
+# }
+
+# moved {
+#   from = module.apim.module.apim_v2_web_wallet_api.azurerm_api_management_api_policy.this[0]
+#   to   = module.apim.azurerm_api_management_api_policy.io_web
+# }
+
+# moved {
+#   from = module.apim_itn.module.apim_v2_web_wallet_api.azurerm_api_management_api_policy.this[0]
+#   to   = module.apim_itn.azurerm_api_management_api_policy.io_web
+# }
+
+# moved {
+#   from = module.apim.module.apim_v2_web_wallet_api.azurerm_api_management_product_api.this["io-web-api"]
+#   to   = module.apim.azurerm_api_management_product_api.io_web
+# }
+
+# moved {
+#   from = module.apim_itn.module.apim_v2_web_wallet_api.azurerm_api_management_product_api.this["io-web-api"]
+#   to   = module.apim_itn.azurerm_api_management_product_api.io_web
+# }
