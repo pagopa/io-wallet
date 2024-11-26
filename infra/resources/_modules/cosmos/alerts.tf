1c994674749379b8b49a9c3b4d411773ccac8a87
Andrea Grillo <andrea.grillo@pagopa.it>
2024-11-26 09:31:22 +0100

    [CES-550] Add IAM roles and alerts on cosmos 02 (#312)

--- infra/resources/_modules/cosmos/alerts.tf
+++ infra/resources/_modules/cosmos/alerts.tf
@@ -47,3 +47,53 @@ resource "azurerm_monitor_metric_alert" "cosmos_db_provisioned_throughput_exceed
 
   tags = var.tags
 }
+
+resource "azurerm_monitor_metric_alert" "cosmos_db_provisioned_throughput_exceeded_02" {
+  name                = "[${local.cosmos_02.name}] Provisioned Throughput Exceeded"
+  resource_group_name = var.resource_group_name
+  scopes              = [local.cosmos_02.id]
+  description         = "A collection throughput (RU/s) exceed provisioned throughput, and it's raising 429 errors. Please, consider to increase RU. Runbook: https://pagopa.atlassian.net/wiki/spaces/SIW/pages/1311735955/Runbook"
+  severity            = 0
+  window_size         = "PT5M"
+  frequency           = "PT5M"
+  auto_mitigate       = false
+
+  # Metric info
+  # https://learn.microsoft.com/en-us/azure/azure-monitor/essentials/metrics-supported#microsoftdocumentdbdatabaseaccounts
+  criteria {
+    metric_namespace       = "Microsoft.DocumentDB/databaseAccounts"
+    metric_name            = "TotalRequestUnits"
+    aggregation            = "Total"
+    operator               = "GreaterThan"
+    threshold              = 0
+    skip_metric_validation = false
+
+    dimension {
+      name     = "Region"
+      operator = "Include"
+      values   = [var.location]
+    }
+
+    dimension {
+      name     = "StatusCode"
+      operator = "Include"
+      values   = ["429"]
+    }
+
+    dimension {
+      name     = "CollectionName"
+      operator = "Include"
+      values   = ["*"]
+    }
+  }
+
+  action {
+    action_group_id = var.action_group_wallet_id
+  }
+
+  action {
+    action_group_id = var.action_group_io_id
+  }
+
+  tags = var.tags
+}
