1864ba365263cc4c9910c723382b0ee5ab7d6b44
silvicir <127971996+silvicir@users.noreply.github.com>
2024-11-27 15:16:04 +0100

    fix sticky_app_setting_names

--- infra/resources/_modules/function_apps/function_app_user.tf
+++ infra/resources/_modules/function_apps/function_app_user.tf
@@ -22,9 +22,12 @@ module "function_app_user_02" {
     resource_group_name = var.virtual_network.resource_group_name
   }
 
-  app_settings             = local.function_app_user.app_settings
-  slot_app_settings        = local.function_app_user.slot_app_settings
-  sticky_app_setting_names = local.function_app_user_slot_disabled
+  app_settings      = local.function_app_user.app_settings
+  slot_app_settings = local.function_app_user.slot_app_settings
+  sticky_app_setting_names = [
+    for to_disable in local.function_app_user_slot_disabled :
+    format("AzureWebJobs.%s.Disabled", to_disable)
+  ]
 
   tier = "xxl"
 
