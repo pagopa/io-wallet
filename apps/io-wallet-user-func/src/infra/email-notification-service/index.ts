f7c1eb9dd1fb1553f518c81820e20d6cfc109a5c
christianatzeni <christian.alessandro.atzeni@it.ey.com>
2024-12-09 15:35:54 +0100

    Using the getMailerTransporter() function (from @pagopa/io-functions-common) directly

--- apps/io-wallet-user-func/src/infra/email-notification-service/index.ts
+++ apps/io-wallet-user-func/src/infra/email-notification-service/index.ts
@@ -8,31 +8,35 @@ import {
   sendMail,
 } from "@pagopa/io-functions-commons/dist/src/mailer";
 import * as TE from "fp-ts/lib/TaskEither";
+import { pipe } from "fp-ts/lib/function";
 
 export class EmailNotificationService implements IEmailNotificationService {
   #configuration: MailConfig;
 
-  getTransporter = () =>
-    getMailerTransporter({
-      MAIL_FROM: this.#configuration.mailSender,
-      MAIL_TRANSPORTS: undefined, // required to comply with the constraints imposed by the MailupMailerConfig type from @pagopa/io-functions-commons
-      MAILHOG_HOSTNAME: undefined, // required to comply with the constraints imposed by the MailupMailerConfig type from @pagopa/io-functions-commons
-      MAILUP_SECRET: this.#configuration.mailupSecret,
-      MAILUP_USERNAME: this.#configuration.mailupUsername,
-      NODE_ENV: "production", // required to comply with the constraints imposed by the MailupMailerConfig type from @pagopa/io-functions-commons
-      SENDGRID_API_KEY: undefined, // required to comply with the constraints imposed by the MailupMailerConfig type from @pagopa/io-functions-commons
-    });
-
   sendEmail: (
     params: SendEmailNotificationParams,
   ) => TE.TaskEither<Error, void> = (params: SendEmailNotificationParams) =>
-    sendMail(this.getTransporter(), {
-      from: this.#configuration.mailSender,
-      html: params.html,
-      subject: params.subject,
-      text: params.text,
-      to: params.to,
-    });
+    pipe(
+      // required to comply with the constraints imposed by the MailupMailerConfig type from @pagopa/io-functions-commons
+      {
+        MAIL_FROM: this.#configuration.mailSender,
+        MAIL_TRANSPORTS: undefined,
+        MAILHOG_HOSTNAME: undefined,
+        MAILUP_SECRET: this.#configuration.mailupSecret,
+        MAILUP_USERNAME: this.#configuration.mailupUsername,
+        NODE_ENV: "production",
+        SENDGRID_API_KEY: undefined,
+      },
+      getMailerTransporter,
+      (transporter) =>
+        sendMail(transporter, {
+          from: this.#configuration.mailSender,
+          html: params.html,
+          subject: params.subject,
+          text: params.text,
+          to: params.to,
+        }),
+    );
 
   constructor(mailConfig: MailConfig) {
     this.#configuration = mailConfig;
