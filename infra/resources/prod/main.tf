4f008c634d4d0652f4bfd7d8c69ad2216fdf5e91
christianatzeni <christian.alessandro.atzeni@it.ey.com>
2024-12-13 15:32:09 +0100

    Fixing a type in the revocation_queue_name_02 identifier

--- infra/resources/prod/main.tf
+++ infra/resources/prod/main.tf
@@ -117,7 +117,7 @@ module "function_apps" {
   application_insights_connection_string = data.azurerm_application_insights.common.connection_string
 
   send_email_queue_name                            = module.storage_accounts.revocation_queue_name_02.name
-  validate_wallet_instance_certificates_queue_name = module.storage_accounts.revocation_queue_name_02q.name
+  validate_wallet_instance_certificates_queue_name = module.storage_accounts.revocation_queue_name_02.name
   pid_issuer_revoke_api_queue_name                 = module.storage_accounts.pid_issuer_revoke_api_queue_name_01.name
 
   user_func    = local.user_func
