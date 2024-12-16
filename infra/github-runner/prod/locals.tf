b08c385df80e3a9a59bfe57e3621be7be27b1420
Krusty93 <andera.grillo@pagopa.it>
2024-12-16 19:01:13 +0100

    update gh runner

--- infra/github-runner/prod/locals.tf
+++ infra/github-runner/prod/locals.tf
@@ -1,14 +1,19 @@
 locals {
-  prefix    = "io"
-  env_short = "p"
+  prefix         = "io"
+  env_short      = "p"
+  location       = "italynorth"
+  location_short = "itn"
+  domain         = "wallet"
+  project        = "${local.prefix}-${local.env_short}-${local.location_short}"
+
   repo_name = "io-wallet"
 
   tags = {
-    CostCenter     = "TS310 - PAGAMENTI & SERVIZI"
+    CostCenter     = "TS700 - ENGINEERING"
     CreatedBy      = "Terraform"
     Environment    = "Prod"
     Owner          = "IO"
-    ManagementTeam = "IO Comunicazione"
+    ManagementTeam = "IO Wallet"
     Source         = "https://github.com/pagopa/io-wallet/infra/github-runner/prod"
   }
 }
