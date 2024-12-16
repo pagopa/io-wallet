d7e72af6f42a17e7789f0878465c2964926fdb1c
silvicir <127971996+silvicir@users.noreply.github.com>
2024-12-16 17:38:51 +0100

    revert .tf files

--- infra/resources/prod/main.tf
+++ infra/resources/prod/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "<= 3.117.0"
+      version = "<= 3.106.1"
     }
 
     azuread = {
@@ -118,6 +118,7 @@ module "function_apps" {
 
   revocation_queue_name                            = module.storage_accounts.revocation_queue_name_02.name
   validate_wallet_instance_certificates_queue_name = module.storage_accounts.revocation_queue_name_02.name
+  pid_issuer_revoke_api_queue_name                 = module.storage_accounts.pid_issuer_revoke_api_queue_name_01.name
 
   user_func    = local.user_func
   support_func = local.support_func
