3618c8b4df11b87149fbb84670759f8b8e85e298
christianatzeni <christian.alessandro.atzeni@it.ey.com>
2024-12-17 16:27:26 +0100

    Applying the correct email template (from @pagopa/io-app-email-templates) for the WI creation

--- apps/io-wallet-user-func/src/infra/handlers/send-email-on-wallet-instance-creation.ts
+++ apps/io-wallet-user-func/src/infra/handlers/send-email-on-wallet-instance-creation.ts
@@ -1,14 +1,14 @@
 import {
-  // WALLET_ACTIVATION_EMAIL_FAQ_LINK,
-  // WALLET_ACTIVATION_EMAIL_HANDLE_ACCESS_LINK,
+  WALLET_ACTIVATION_EMAIL_FAQ_LINK,
+  WALLET_ACTIVATION_EMAIL_HANDLE_ACCESS_LINK,
   WALLET_ACTIVATION_EMAIL_SUBJECT,
   WALLET_ACTIVATION_EMAIL_TEXT,
 } from "@/app/config";
 import { getUserEmailByFiscalCode, sendEmailToUser } from "@/email";
 import * as H from "@pagopa/handler-kit";
-// [SIW-1560] - work in progress
-// import { apply as htmlTemplate } from "@pagopa/io-app-email-templates/WalletInstanceCreation/index";
+import { apply as htmlTemplate } from "@pagopa/io-app-email-templates/WalletInstanceCreation/index";
 import { FiscalCode } from "@pagopa/ts-commons/lib/strings";
+import { ValidUrl } from "@pagopa/ts-commons/lib/url";
 import { pipe } from "fp-ts/function";
 import * as RTE from "fp-ts/lib/ReaderTaskEither";
 
@@ -18,13 +18,10 @@ export const SendEmailOnWalletInstanceCreationHandler = H.of(
       getUserEmailByFiscalCode(fiscalCode),
       RTE.chainW((emailAddress) =>
         sendEmailToUser({
-          // [SIW-1560] - get the WalletInstanceCreation email template from @pagopa/io-wallet-email
-          html: "",
-          // [SIW-1560] - work in progress
-          // htmlTemplate(
-          //  WALLET_ACTIVATION_EMAIL_FAQ_LINK,
-          //  WALLET_ACTIVATION_EMAIL_HANDLE_ACCESS_LINK,
-          // ),
+          html: htmlTemplate(
+            { href: WALLET_ACTIVATION_EMAIL_FAQ_LINK } as ValidUrl,
+            { href: WALLET_ACTIVATION_EMAIL_HANDLE_ACCESS_LINK } as ValidUrl,
+          ),
           subject: WALLET_ACTIVATION_EMAIL_SUBJECT,
           text: WALLET_ACTIVATION_EMAIL_TEXT,
           to: emailAddress,
