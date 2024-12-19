87bf534aa32753d3e5afd78781e85317037c9d5c
silvicir <127971996+silvicir@users.noreply.github.com>
2024-12-19 10:27:49 +0100

    fix email

--- apps/io-wallet-user-func/src/app/config.ts
+++ apps/io-wallet-user-func/src/app/config.ts
@@ -61,7 +61,7 @@ export const MailConfig = t.type({
 export type MailConfig = t.TypeOf<typeof MailConfig>;
 
 export const WALLET_ACTIVATION_EMAIL_TITLE =
-  "IT Wallet - Aggiungi i tuoi documenti al Portafoglio di IO";
+  "Documenti su IO - Aggiungi i tuoi documenti al Portafoglio";
 export const WALLET_ACTIVATION_EMAIL_FAQ_LINK =
   "https://io.italia.it/documenti-su-io/faq/";
 export const WALLET_ACTIVATION_EMAIL_HANDLE_ACCESS_LINK =
