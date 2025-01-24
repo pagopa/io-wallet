8fabbc812802e26b045301a7f19471776cf29c39
Andrea Grillo <andrea.grillo@pagopa.it>
2025-01-24 15:10:58 +0100

    [CES-589] IAM: replace io-p-adgroup-developers with io-p-adgroup-wallet-admins (#354)
    Co-authored-by: Krusty93 <andera.grillo@pagopa.it>

--- infra/github-runner/prod/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "container_app_job" {
-  value = {
-    id                  = module.container_app_job_selfhosted_runner.container_app_job.id
-    name                = module.container_app_job_selfhosted_runner.container_app_job.name
-    resource_group_name = module.container_app_job_selfhosted_runner.container_app_job.resource_group_name
-  }
-}
