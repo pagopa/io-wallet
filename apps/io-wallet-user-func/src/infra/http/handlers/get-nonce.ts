abfdf32747aa98e22ed566086fd3f52cd2e2b6a8
christianatzeni <christian.alessandro.atzeni@it.ey.com>
2025-02-03 17:02:57 +0100

    Adding a body with fiscalCode to the GetNonce API

--- apps/io-wallet-user-func/src/infra/http/handlers/get-nonce.ts
+++ apps/io-wallet-user-func/src/infra/http/handlers/get-nonce.ts
@@ -1,11 +1,25 @@
 import { generateNonce, insertNonce } from "@/nonce";
 import * as H from "@pagopa/handler-kit";
+import { FiscalCode } from "@pagopa/ts-commons/lib/strings";
+import * as E from "fp-ts/lib/Either";
 import * as RTE from "fp-ts/lib/ReaderTaskEither";
 import { pipe } from "fp-ts/lib/function";
+import * as t from "io-ts";
 import { sendTelemetryException } from "io-wallet-common/infra/azure/appinsights/telemetry";
 import { logErrorAndReturnResponse } from "io-wallet-common/infra/http/error";
 
-export const GetNonceHandler = H.of(() =>
+const GetNonceBody = t.type({
+  fiscalCode: FiscalCode,
+});
+
+type GetNonceBody = t.TypeOf<typeof GetNonceBody>;
+
+const requireGetNonceBody: (
+  req: H.HttpRequest,
+) => E.Either<Error, GetNonceBody> = (req) =>
+  pipe(req.body, H.parse(GetNonceBody, "Invalid body supplied"));
+
+export const GetNonceHandler = H.of((req: H.HttpRequest) =>
   pipe(
     generateNonce,
     RTE.fromIOEither,
@@ -15,6 +29,11 @@ export const GetNonceHandler = H.of(() =>
     RTE.orElseFirstW((error) =>
       pipe(
         sendTelemetryException(error, {
+          fiscalCode: pipe(
+            req,
+            requireGetNonceBody,
+            E.map(({ fiscalCode }) => fiscalCode),
+          ),
           functionName: "getNonce",
         }),
         RTE.fromReader,
