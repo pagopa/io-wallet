967dc75012c5cb02462db9778a19bc6649423cd8
Christian Calabrese <christian.calabrese@pagopa.it>
2025-02-12 11:01:27 +0100

    [CES-742] Create new dns zone wallet.io.pagopa.it (#380)

--- infra/resources/_modules/cdn/locals.tf
+++ infra/resources/_modules/cdn/locals.tf
@@ -1,7 +1,8 @@
 locals {
   dns_zone_name_resource_group_name = "io-p-rg-external"
 
-  dns_name          = "wallet"
-  cdn_dns_zone_name = "io.pagopa.it"
-  cdn_hostname      = "${local.dns_name}.${local.cdn_dns_zone_name}"
+  dns_name                 = "wallet"
+  cdn_dns_zone_name        = "io.pagopa.it"
+  wallet_cdn_dns_zone_name = "wallet.io.pagopa.it"
+  cdn_hostname             = "${local.dns_name}.${local.cdn_dns_zone_name}"
 }
