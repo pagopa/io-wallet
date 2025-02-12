8e073c6a1c416e19a6e5d539145ea3effca32862
Christian Calabrese <christian.calabrese@pagopa.it>
2025-02-12 11:01:27 +0100

    [CES-742] Create new dns zone wallet.io.pagopa.it (#380)

--- infra/resources/_modules/cdn/dns.tf
+++ infra/resources/_modules/cdn/dns.tf
@@ -7,3 +7,24 @@ resource "azurerm_dns_cname_record" "this" {
 
   tags = var.tags
 }
+
+resource "azurerm_dns_a_record" "this" {
+  name                = "@"
+  zone_name           = local.wallet_cdn_dns_zone_name
+  resource_group_name = local.dns_zone_name_resource_group_name
+  ttl                 = 30
+  target_resource_id  = azurerm_cdn_endpoint.this.id
+  tags                = var.tags
+}
+
+# TXT for Maven verification
+resource "azurerm_dns_txt_record" "wallet_io_pagopa_it" {
+  name                = "@"
+  zone_name           = local.wallet_cdn_dns_zone_name
+  resource_group_name = local.dns_zone_name_resource_group_name
+  ttl                 = 3600
+  record {
+    value = "qidvirtenu"
+  }
+  tags = var.tags
+}
