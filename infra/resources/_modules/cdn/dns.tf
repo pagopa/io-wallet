771946c90d404217e5b5afa8fdab7be75697966b
hevelius <mario.perrotta@pagopa.it>
2025-02-12 16:50:29 +0100

    chore: add dns txt record ns cbor and proximity verification

--- infra/resources/_modules/cdn/dns.tf
+++ infra/resources/_modules/cdn/dns.tf
@@ -18,3 +18,27 @@ resource "azurerm_dns_txt_record" "wallet_io_pagopa_it" {
   }
   tags = var.tags
 }
+
+# TXT for cbor Maven namespace verification
+resource "azurerm_dns_txt_record" "cbor_wallet_io_pagopa_it" {
+  name                = "@"
+  zone_name           = local.wallet_cdn_dns_zone_name
+  resource_group_name = local.dns_zone_name_resource_group_name
+  ttl                 = 3600
+  record {
+    value = "mku7fqer6d"
+  }
+  tags = var.tags
+}
+
+# TXT for proximity Maven namespace verification
+resource "azurerm_dns_txt_record" "proximity_wallet_io_pagopa_it" {
+  name                = "@"
+  zone_name           = local.wallet_cdn_dns_zone_name
+  resource_group_name = local.dns_zone_name_resource_group_name
+  ttl                 = 3600
+  record {
+    value = "sh4ibgibuy"
+  }
+  tags = var.tags
+}
