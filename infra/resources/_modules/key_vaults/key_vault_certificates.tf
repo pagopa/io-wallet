a46a275896ad454bcc6407c8a5dd8509b7309ba3
silvicir <127971996+silvicir@users.noreply.github.com>
2025-06-03 09:54:34 +0200

    removed comments

--- infra/resources/_modules/key_vaults/key_vault_certificates.tf
+++ infra/resources/_modules/key_vaults/key_vault_certificates.tf
@@ -1,19 +1,19 @@
-# data "azurerm_subscription" "current" {}
+data "azurerm_subscription" "current" {}
 
-# module "key_vault_role_assignments" {
-#   source = "github.com/pagopa-dx/terraform-azurerm-azure-role-assignments//modules/key_vault"
+module "key_vault_role_assignments" {
+  source = "github.com/pagopa-dx/terraform-azurerm-azure-role-assignments//modules/key_vault"
 
-#   principal_id = var.cdn_principal_id
+  principal_id = var.cdn_principal_id
 
-#   subscription_id = data.azurerm_subscription.current.id
+  subscription_id = data.azurerm_subscription.current.id
 
-#   key_vault = [{
-#     name                = var.key_vault_certificates.name
-#     resource_group_name = var.key_vault_certificates.resource_group_name
-#     description         = "It is required so the CDN can get the certificates stored in the key vault"
+  key_vault = [{
+    name                = var.key_vault_certificates.name
+    resource_group_name = var.key_vault_certificates.resource_group_name
+    description         = "It is required so the CDN can get the certificates stored in the key vault"
 
-#     roles = {
-#       certificates = "reader"
-#     }
-#   }]
-# }
+    roles = {
+      certificates = "reader"
+    }
+  }]
+}
