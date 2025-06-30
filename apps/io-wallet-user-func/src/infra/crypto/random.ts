cb00dc71f6b3f44c63dcc8f1e05cca74cd7ed612
silvicir <127971996+silvicir@users.noreply.github.com>
2025-06-30 13:54:33 +0200

    removed unused function

--- apps/io-wallet-user-func/src/infra/crypto/random.ts
+++ apps/io-wallet-user-func/src/infra/crypto/random.ts
@@ -7,14 +7,6 @@ const generateRandomBytes: IOE.IOEither<Error, Buffer> = IOE.tryCatch(
   (error) => new Error(`Failed to generate random bytes: ${error}`),
 );
 
-export const generateRandomUint8Array: IOE.IOEither<
-  Error,
-  Uint8Array<ArrayBufferLike>
-> = pipe(
-  generateRandomBytes,
-  IOE.map((buf) => new Uint8Array(buf)),
-);
-
 export const generateRandomHex: IOE.IOEither<Error, string> = pipe(
   generateRandomBytes,
   IOE.map((buf) => buf.toString("hex")),
