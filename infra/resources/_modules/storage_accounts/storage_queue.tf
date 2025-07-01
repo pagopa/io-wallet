30c0e3a13c72b2cca704b76dff1c6bb0a631b2b9
silvicir <127971996+silvicir@users.noreply.github.com>
2025-07-01 16:07:22 +0200

    infra: added queues creation and revocation wallet instance
    Refs: #456 SIW-2499

--- infra/resources/_modules/storage_accounts/storage_queue.tf
+++ infra/resources/_modules/storage_accounts/storage_queue.tf
@@ -17,3 +17,13 @@ resource "azurerm_storage_queue" "wallet-instance-revocation-email-queue-01" {
   name                 = "wallet-instance-revocation-email-queue-01"
   storage_account_name = module.storage_account.name
 }
+
+resource "azurerm_storage_queue" "wallet-instance-creation-email-queue-uat" {
+  name                 = "wallet-instance-creation-email-queue-01"
+  storage_account_name = module.storage_account_uat.name
+}
+
+resource "azurerm_storage_queue" "wallet-instance-revocation-email-queue-uat" {
+  name                 = "wallet-instance-revocation-email-queue-01"
+  storage_account_name = module.storage_account_uat.name
+}
