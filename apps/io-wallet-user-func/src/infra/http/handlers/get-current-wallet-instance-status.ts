7fdfa252ca93d54197b51c099647ff2e97eae3cd
silvicir <127971996+silvicir@users.noreply.github.com>
2025-09-05 09:51:40 +0200

    fix lint and typecheck

--- apps/io-wallet-user-func/src/infra/http/handlers/get-current-wallet-instance-status.ts
+++ apps/io-wallet-user-func/src/infra/http/handlers/get-current-wallet-instance-status.ts
@@ -2,7 +2,6 @@ import { getCurrentWalletInstance } from "@/wallet-instance";
 import * as H from "@pagopa/handler-kit";
 import * as RTE from "fp-ts/lib/ReaderTaskEither";
 import { pipe } from "fp-ts/lib/function";
-import { sendTelemetryException } from "io-wallet-common/infra/azure/appinsights/telemetry";
 import { logErrorAndReturnResponse } from "io-wallet-common/infra/http/error";
 
 import { WalletInstanceToStatusApiModel } from "../encoders/wallet-instance";
