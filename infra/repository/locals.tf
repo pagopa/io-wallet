fd46de770eaf6a46e8c25e0a2f3d99d26e55365c
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-09-23 16:53:24 +0000

    Remove unused locals

--- infra/repository/locals.tf
+++ infra/repository/locals.tf
@@ -1,43 +1,4 @@
 locals {
-  prefix          = "io"
-  env_short       = "p"
-  location        = "italynorth"
-  domain          = "wallet"
-  instance_number = "01"
-
-  adgroups = {
-    admins_name = "io-p-adgroup-wallet-admins"
-    devs_name   = "io-p-adgroup-wallet-developers"
-  }
-
-  runner = {
-    cae_name                = "${local.prefix}-${local.env_short}-itn-github-runner-cae-01"
-    cae_resource_group_name = "${local.prefix}-${local.env_short}-itn-github-runner-rg-01"
-    secret = {
-      kv_name                = "${local.prefix}-${local.env_short}-kv-common"
-      kv_resource_group_name = "${local.prefix}-${local.env_short}-rg-common"
-    }
-  }
-
-  apim = {
-    name                = "${local.prefix}-${local.env_short}-itn-apim-01"
-    resource_group_name = "${local.prefix}-${local.env_short}-itn-common-rg-01"
-  }
-
-  vnet = {
-    name                = "${local.prefix}-${local.env_short}-itn-common-vnet-01"
-    resource_group_name = "${local.prefix}-${local.env_short}-itn-common-rg-01"
-  }
-
-  dns_zones = {
-    resource_group_name = "${local.prefix}-${local.env_short}-rg-common"
-  }
-
-  tf_storage_account = {
-    name                = "iopitntfst001"
-    resource_group_name = "terraform-state-rg"
-  }
-
   repository = {
     name                     = "io-wallet"
     description              = "Wallet Provider implementation for EUDI Wallet and IT-Wallet for App IO"
@@ -48,19 +9,5 @@ locals {
     opex_cd_policy_branches  = ["master"]
     app_cd_policy_branches   = ["master"]
     jira_boards_ids          = ["CES", "SIW"]
-  }
-
-  key_vault = {
-    name                = "io-p-kv-common"
-    resource_group_name = "io-p-rg-common"
-  }
-
-  tags = {
-    CreatedBy      = "Terraform"
-    Environment    = "Prod"
-    BusinessUnit   = "IT Wallet"
-    ManagementTeam = "IO Wallet"
-    Source         = "https://github.com/pagopa/io-wallet/blob/main/infra/repository"
-    CostCenter     = "TS000 - Tecnologia e Servizi"
   }
 }
