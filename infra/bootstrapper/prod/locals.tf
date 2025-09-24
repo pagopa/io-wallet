94791b5387ba52de0aeafb41c754c6ef18ee7512
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-09-24 07:28:55 +0000

    Remove unused properties

--- infra/bootstrapper/prod/locals.tf
+++ infra/bootstrapper/prod/locals.tf
@@ -40,14 +40,6 @@ locals {
 
   repository = {
     name                     = "io-wallet"
-    description              = "Wallet Provider implementation for EUDI Wallet and IT-Wallet for App IO"
-    topics                   = ["eudiw", "io", "it-wallet"]
-    reviewers_teams          = ["io-wallet", "engineering-team-cloud-eng"]
-    default_branch_name      = "master"
-    infra_cd_policy_branches = ["master"]
-    opex_cd_policy_branches  = ["master"]
-    app_cd_policy_branches   = ["master"]
-    jira_boards_ids          = ["CES", "SIW"]
   }
 
   key_vault = {
