4c14de8c8806785537e25fb6927152e2f54524c7
Luca Cavallaro <luca.cavallaro@pagopa.it>
2025-10-02 15:15:34 +0000

    Change tf state location

--- infra/bootstrapper/prod/main.tf
+++ infra/bootstrapper/prod/main.tf
@@ -20,7 +20,7 @@ terraform {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "iopitntfst001"
     container_name       = "terraform-state"
-    key                  = "io-wallet.repository.tfstate"
+    key                  = "io-wallet.bootstrapper.prod.tfstate"
     use_azuread_auth     = true
   }
 }
