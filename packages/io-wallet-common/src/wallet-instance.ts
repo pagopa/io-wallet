3af54b626fee1cae33f17f855df75e878528fbf4
silvicir <127971996+silvicir@users.noreply.github.com>
2025-10-03 10:39:54 +0200

    updated common package

--- packages/io-wallet-common/src/wallet-instance.ts
+++ packages/io-wallet-common/src/wallet-instance.ts
@@ -3,6 +3,7 @@ import { FiscalCode, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import * as t from "io-ts";
 
 import {
+  AndroidDeviceDetails,
   DeviceDetails,
   DeviceDetailsStringOsPatchLevel,
 } from "./device-details";
@@ -55,3 +56,19 @@ export const WalletInstance = t.union([
 ]);
 
 export type WalletInstance = t.TypeOf<typeof WalletInstance>;
+
+export const WalletInstanceValidWithAndroidCertificatesChain = t.intersection([
+  WalletInstanceValid,
+  t.type({
+    deviceDetails: t.intersection([
+      AndroidDeviceDetails,
+      t.type({
+        x509Chain: t.array(t.string),
+      }),
+    ]),
+  }),
+]);
+
+export type WalletInstanceValidWithAndroidCertificatesChain = t.TypeOf<
+  typeof WalletInstanceValidWithAndroidCertificatesChain
+>;
