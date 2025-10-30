bb07e4ce8b0aa6ab5823664e93145de6ca9da340
silvicir <127971996+silvicir@users.noreply.github.com>
2025-10-30 14:48:49 +0100

    fix build

--- apps/io-wallet-support-func/src/app/config.ts
+++ apps/io-wallet-support-func/src/app/config.ts
@@ -3,17 +3,12 @@ import { sequenceS } from "fp-ts/lib/Apply";
 import * as RE from "fp-ts/lib/ReaderEither";
 import * as t from "io-ts";
 import {
-  AzureAppInsightsConfig,
-  getAzureAppInsightsConfigFromEnvironment,
-} from "io-wallet-common/infra/azure/appinsights/config";
-import {
   AzureCosmosConfig,
   getAzureCosmosConfigFromEnvironment,
 } from "io-wallet-common/infra/azure/cosmos/config";
 
 const Config = t.type({
   azure: t.type({
-    appInsights: AzureAppInsightsConfig,
     cosmos: AzureCosmosConfig,
   }),
 });
@@ -26,12 +21,10 @@ export const getConfigFromEnvironment: RE.ReaderEither<
   Config
 > = pipe(
   sequenceS(RE.Apply)({
-    appInsights: getAzureAppInsightsConfigFromEnvironment,
     cosmos: getAzureCosmosConfigFromEnvironment,
   }),
-  RE.map(({ appInsights, cosmos }) => ({
+  RE.map(({ cosmos }) => ({
     azure: {
-      appInsights,
       cosmos,
     },
   })),
