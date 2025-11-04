7f16a4a9bb388943cf5f9062db946c9050f0cc29
silvicir <127971996+silvicir@users.noreply.github.com>
2025-11-04 16:15:34 +0100

    updated code

--- apps/io-wallet-user-func/src/infra/telemetry/index.ts
+++ apps/io-wallet-user-func/src/infra/telemetry/index.ts
@@ -12,23 +12,26 @@ const DEFAULT_LOGGER = "ApplicationInsightsLogger";
 export const sendTelemetryException: (
   properties?: Record<string, unknown>,
 ) => (error: Error) => E.Either<Error, void> = (properties) => (error) =>
-  E.tryCatch(() => {
-    logs.getLogger(DEFAULT_LOGGER).emit({
-      attributes: {
-        [ATTR_EXCEPTION_MESSAGE]: error.message,
-        [ATTR_EXCEPTION_STACKTRACE]: error.stack,
-        [ATTR_EXCEPTION_TYPE]: error.name,
-        ...properties,
-      },
-      severityNumber: SeverityNumber.ERROR,
-      severityText: "ERROR",
-    });
-  }, E.toError);
+  E.tryCatch(
+    () =>
+      logs.getLogger(DEFAULT_LOGGER).emit({
+        attributes: {
+          [ATTR_EXCEPTION_MESSAGE]: error.message,
+          [ATTR_EXCEPTION_STACKTRACE]: error.stack,
+          [ATTR_EXCEPTION_TYPE]: error.name,
+          ...properties,
+        },
+        severityNumber: SeverityNumber.ERROR,
+        severityText: "ERROR",
+      }),
+    E.toError,
+  );
 
 export const sendTelemetryCustomEvent: (event: {
   name: string;
   properties: Record<string, string>;
 }) => E.Either<Error, void> = ({ name, properties }) =>
-  E.tryCatch(() => {
-    emitCustomEvent(name, properties)(DEFAULT_LOGGER);
-  }, E.toError);
+  E.tryCatch(
+    () => emitCustomEvent(name, properties)(DEFAULT_LOGGER),
+    E.toError,
+  );
