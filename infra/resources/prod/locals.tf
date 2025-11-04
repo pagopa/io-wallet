26c53e17de50b93d06317240ec984516effb2b5e
silvicir <127971996+silvicir@users.noreply.github.com>
2025-11-04 11:04:39 +0100

    updated tf

--- infra/resources/prod/locals.tf
+++ infra/resources/prod/locals.tf
@@ -77,7 +77,7 @@ locals {
         key_vault_secret_name = module.storage_accounts.wallet.connection_string_secret_name
       },
       {
-        name                  = "AppInsightsConnectionString"
+        name                  = "APPLICATIONINSIGHTS_CONNECTION_STRING"
         key_vault_secret_name = "AppInsightsConnectionString"
       },
       {
