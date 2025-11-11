678eb19e8e755f18bbb175b9eee7275bd84c803e
silvicir <127971996+silvicir@users.noreply.github.com>
2025-11-11 15:36:10 +0100

    removed key vault secret creation

--- infra/resources/_modules/function_apps/key_vault_secrets.tf
+++ infra/resources/_modules/function_apps/key_vault_secrets.tf
@@ -1,12 +1,4 @@
 # tfsec:ignore:azure-keyvault-ensure-secret-expiry
-resource "azurerm_key_vault_secret" "func_user_default_key" {
-  name         = "funciowallet-KEY-APPBACKEND"
-  value        = data.azurerm_function_app_host_keys.this.default_function_key
-  key_vault_id = var.key_vault_id
-  content_type = "token"
-}
-
-# tfsec:ignore:azure-keyvault-ensure-secret-expiry
 resource "azurerm_key_vault_secret" "func_user_uat_default_key" {
   name         = "funciowalletuat-KEY-APPBACKEND"
   value        = data.azurerm_function_app_host_keys.user_uat_func.default_function_key
