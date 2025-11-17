92217cfe017c0aaa31177a2d31b9b8ab695890e9
silvicir <127971996+silvicir@users.noreply.github.com>
2025-11-17 14:41:46 +0100

    updated user uat function app node version to 22

--- infra/resources/_modules/function_apps/function_app_user_uat.tf
+++ infra/resources/_modules/function_apps/function_app_user_uat.tf
@@ -13,7 +13,7 @@ module "function_app_user_uat" {
 
   resource_group_name = var.resource_group_name
   health_check_path   = "/api/v1/wallet/health"
-  node_version        = 20
+  node_version        = 22
 
   subnet_cidr                          = var.cidr_subnet_user_uat_func
   subnet_pep_id                        = var.private_endpoint_subnet_id
