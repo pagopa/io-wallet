c5d9037fd3793a4bd049c300e23d0f06a0a3d6f0
Andrea Grillo <andrea.grillo@pagopa.it>
2025-12-02 13:31:17 +0100

    Rename CMK identity on PSN tenant (#571)

--- infra/resources/psn/prod/main.tf
+++ infra/resources/psn/prod/main.tf
@@ -2,7 +2,7 @@ module "ids" {
   source = "../../_modules/identities"
 
   environment = merge(local.environment, {
-    name = "cmk"
+    name = "data-cmk"
   })
   resource_group_name = data.azurerm_resource_group.wallet.name
 
