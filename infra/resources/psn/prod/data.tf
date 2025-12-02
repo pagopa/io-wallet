facdf019bc1072a5979419c644b140bc02774809
Krusty93 <andrea.grillo@pagopa.it>
2025-12-02 13:29:18 +0100

    remove empty value

--- infra/resources/psn/prod/data.tf
+++ infra/resources/psn/prod/data.tf
@@ -2,7 +2,6 @@ data "azurerm_resource_group" "wallet" {
   name = provider::dx::resource_name(merge(
     local.environment,
     {
-      domain        = ""
       name          = "wallet",
       resource_type = "resource_group"
     }
