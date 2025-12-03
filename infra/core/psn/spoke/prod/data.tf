07adf5895dcf46316791e81e06a11ba30108a1e0
Andrea Grillo <andrea.grillo@pagopa.it>
2025-12-03 11:37:56 +0100

    Secure Terraform state file by closing Storage Account connection (#576)

--- infra/core/psn/spoke/prod/data.tf
+++ infra/core/psn/spoke/prod/data.tf
@@ -1,3 +1,10 @@
 data "azuread_group" "itwallet" {
   display_name = "pagopa-ITWallet"
 }
+
+data "azurerm_private_dns_zone" "blob" {
+  provider = azurerm.hub
+
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = local.networking.hub.resource_group_name
+}
