93386e15d4fb55cedda4250f7a466ba784bb56e5
Krusty93 <andrea.grillo@pagopa.it>
2026-01-07 16:55:11 +0100

    make appgw private with new psn rules

--- infra/core/psn/hub/prod/data.tf
+++ infra/core/psn/hub/prod/data.tf
@@ -2,17 +2,24 @@ data "azurerm_client_config" "current" {}
 
 data "azuread_application_published_app_ids" "well_known" {}
 
+data "azurerm_web_application_firewall_policy" "appgw" {
+  provider = azurerm.hub
+
+  name                = "pagopa-app-gw-italynorth"
+  resource_group_name = azurerm_resource_group.network.name
+}
+
 data "azurerm_virtual_network" "hub" {
   provider = azurerm.hub
 
   name                = "pagopa-hub-italynorth"
-  resource_group_name = "pagopa-rg-hub-network-italynorth"
+  resource_group_name = azurerm_resource_group.network.name
 }
 
 data "azurerm_subnet" "hub_gateway_subnet" {
   provider = azurerm.hub
 
-  name                 = "ApplicationGatewaySubnet"
+  name                 = "ApplicationGatewaySubnet1"
   virtual_network_name = data.azurerm_virtual_network.hub.name
   resource_group_name  = data.azurerm_virtual_network.hub.resource_group_name
 }
@@ -37,5 +44,5 @@ data "azurerm_public_ip" "appgw" {
   provider = azurerm.hub
 
   name                = "pagopa-ApplicationGW-PublicIp"
-  resource_group_name = "pagopa-rg-hub-network-italynorth"
+  resource_group_name = azurerm_resource_group.network.name
 }
