f3299ea6af464270a613ec7c4dc1e6da9ef67778
Silvia Cirulli <silvia.cirulli@pagopa.it>
2026-02-03 16:03:09 +0100

    removed StorageConnectionString env var

--- infra/resources/_modules/function_apps/locals.tf
+++ infra/resources/_modules/function_apps/locals.tf
@@ -130,7 +130,6 @@ locals {
         PidIssuerApiClientPrivateKey  = "@Microsoft.KeyVault(VaultName=${var.key_vault_wallet_name};SecretName=PidIssuerApiClientPrivateKeyPre)"
         GoogleAppCredentialsEncoded   = "@Microsoft.KeyVault(VaultName=${var.key_vault_wallet_name};SecretName=GoogleAppCredentialsEncoded)"
         AllowedDeveloperUsers         = "@Microsoft.KeyVault(VaultName=${var.key_vault_wallet_name};SecretName=AllowedDeveloperUsers)"
-        StorageConnectionString       = var.is_psn ? null : "@Microsoft.KeyVault(VaultName=${var.key_vault_wallet_name};SecretName=StorageUatConnectionString)"
       }
     )
   }
