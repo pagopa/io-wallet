d83ae143353507ab2d958b989d5cc84aaf587b3e
Silvia Cirulli <silvia.cirulli@pagopa.it>
2026-04-13 11:07:43 +0200

    created new storage account and container for WP EC in PRE

--- infra/resources/_modules/storage_accounts/storage_accounts.tf
+++ infra/resources/_modules/storage_accounts/storage_accounts.tf
@@ -5,6 +5,14 @@ locals {
       resource_type = "storage_account"
     }
   ))
+  trust_uat_storage_account_name = provider::dx::resource_name(merge(
+    var.environment,
+    {
+      environment   = var.u_env_short
+      name          = "trust"
+      resource_type = "storage_account"
+    }
+  ))
   uat_storage_account_name = provider::dx::resource_name(merge(
     var.environment,
     {
@@ -71,6 +79,43 @@ resource "azurerm_storage_account" "common_uat" {
       managed_hsm_key_id        = var.customer_managed_key_url
       user_assigned_identity_id = var.user_assigned_managed_identity_id
     }
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_storage_account" "trust_uat" {
+  name                = local.trust_uat_storage_account_name
+  resource_group_name = var.resource_group_name
+  location            = var.environment.location
+
+  identity {
+    type         = var.user_assigned_managed_identity_id == null ? "SystemAssigned" : "UserAssigned"
+    identity_ids = var.user_assigned_managed_identity_id == null ? null : [var.user_assigned_managed_identity_id]
+  }
+
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "ZRS"
+
+  public_network_access_enabled   = true
+  allow_nested_items_to_be_public = true
+  shared_access_key_enabled       = var.user_assigned_managed_identity_id == null
+  default_to_oauth_authentication = var.user_assigned_managed_identity_id != null
+  https_traffic_only_enabled      = true
+  min_tls_version                 = "TLS1_2"
+
+  dynamic "customer_managed_key" {
+    for_each = var.customer_managed_key_url == null || var.user_assigned_managed_identity_id == null ? [] : [1]
+
+    content {
+      managed_hsm_key_id        = var.customer_managed_key_url
+      user_assigned_identity_id = var.user_assigned_managed_identity_id
+    }
+  }
+
+  blob_properties {
+    versioning_enabled = true
   }
 
   tags = var.tags
