0d54be89f4a5ea49df2b969b6b217aa1fe6f0f68
Silvia Cirulli <silvia.cirulli@pagopa.it>
2026-05-26 10:20:27 +0200

    fix CDN alert query validation and document public storage containers

--- infra/resources/_modules/storage_accounts/storage_containers.tf
+++ infra/resources/_modules/storage_accounts/storage_containers.tf
@@ -1,3 +1,5 @@
+# This container is intentionally public because UAT wallet provider metadata must be publicly retrievable.
+#trivy:ignore:AZU-0007 trivy:ignore:AVD-AZU-0007
 resource "azurerm_storage_container" "wallet_provider" {
   name                  = "wallet-provider"
   storage_account_id    = azurerm_storage_account.trust_uat.id
