d2054880e7c5867b29d4b5e5a0413357b4077c7d
Silvia Cirulli <silvia.cirulli@pagopa.it>
2026-09-15 10:28:53 +0200

    restored WalletProviderIntermediateSigningKeys and WalletProviderLeafSigningKeys

--- infra/resources/psn/prod/locals.tf
+++ infra/resources/psn/prod/locals.tf
@@ -26,6 +26,14 @@ locals {
         key_vault_secret_name = "GoogleAppCredentialsEncoded"
       },
       {
+        name                  = "WalletProviderIntermediateSigningKeys"
+        key_vault_secret_name = "WalletProviderIntermediateSigningKeys"
+      },
+      {
+        name                  = "WalletProviderLeafSigningKeys"
+        key_vault_secret_name = "WalletProviderLeafSigningKeys"
+      },
+      {
         name                  = "WalletAttestationSigningKeys"
         key_vault_secret_name = "WalletAttestationSigningKeys"
       },
